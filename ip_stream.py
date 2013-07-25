ab33250a0d76c24baacc1aabe285a46f4d357754
Victor Pereira <vpereirabr@gmail.com>
2013-07-25 12:15:44 +0200

    we dont have to store the whole pkt in memory

--- ip_stream.py
+++ ip_stream.py
@@ -12,15 +12,13 @@ class IPStream(object):
 	self.inter_arrival_times = [0]
 	self.pkt_count = 1
 	self.len = pkt.len
-        self.pkt = pkt
-        self.shannon_pkt = [shannon(self.get_payload())]
+        self.shannon_pkt = [shannon(self.get_payload(pkt))]
 
   def add(self,pkt):
 	self.pkt_count += 1
 	self.len += pkt.len
 	self.inter_arrival_times.append(pkt.time - self.time)
-	self.pkt = pkt
-	self.shannon_pkt.append(shannon(self.get_payload()))
+	self.shannon_pkt.append(shannon(self.get_payload(pkt)))
 
 
   def avrg_len(self):
@@ -32,12 +30,12 @@ class IPStream(object):
   def avrg_shannon(self):
     return round(mean(self.shannon_pkt),4)
 
-  def get_payload(self):
-    if self.pkt.proto == 1:
-        return str(self.pkt[ICMP].payload)
-    elif self.pkt.proto == 6:
-        return str(self.pkt[TCP].payload)
-    elif self.pkt.proto == 17:
-        return str(self.pkt[UDP].payload)
+  def get_payload(self,pkt):
+    if pkt.proto == 1:
+        return str(pkt[ICMP].payload)
+    elif pkt.proto == 6:
+        return str(pkt[TCP].payload)
+    elif pkt.proto == 17:
+        return str(pkt[UDP].payload)
     else:
         return None
